e632a0ef1a823ac632ab02f4bada4cc1e1eafcfc
mustafa <mustafamjaber@yahoo.com>
2021-05-05 19:19:12 +0200

    finishing the app

---
+++ app.js
@@ -0,0 +1,51 @@
+const arrow = document.querySelector('.arr1');
+const arrow2 = document.querySelector('.arr2');
+const arrow3 = document.querySelector('.arr3');
+const arrow4 = document.querySelector('.arr4');
+const arrow5 = document.querySelector('.arr5');
+const anser = document.querySelector('.anser');
+const anser1 = document.querySelector('.anser1');
+const anser2 = document.querySelector('.anser2');
+const anser3 = document.querySelector('.anser3');
+const anser4 = document.querySelector('.anser4');
+
+arrow.addEventListener('click', () => {
+	//console.log('haloo worled');
+	anser.classList.toggle('cosshow1');
+	/*anser1.classList.toggle('cosshow2');
+	anser2.classList.toggle('cosshow3');
+	anser3.classList.toggle('cosshow4');
+	anser4.classList.toggle('cosshow5');*/
+});
+arrow2.addEventListener('click', () => {
+	//console.log('haloo worled');
+	//anser.classList.toggle('cosshow1');
+	anser1.classList.toggle('cosshow2');
+	//anser2.classList.toggle('cosshow3');
+	//anser3.classList.toggle('cosshow4');
+	//anser4.classList.toggle('cosshow5');
+});
+arrow3.addEventListener('click', () => {
+	//console.log('haloo worled');
+	//anser.classList.toggle('cosshow1');
+	//anser1.classList.toggle('cosshow2');
+	anser2.classList.toggle('cosshow3');
+	//anser3.classList.toggle('cosshow4');
+	//anser4.classList.toggle('cosshow5');
+});
+arrow4.addEventListener('click', () => {
+	//console.log('haloo worled');
+	//anser.classList.toggle('cosshow1');
+	//anser1.classList.toggle('cosshow2');
+	//anser2.classList.toggle('cosshow3');
+	anser3.classList.toggle('cosshow4');
+	//anser4.classList.toggle('cosshow5');
+});
+arrow5.addEventListener('click', () => {
+	//console.log('haloo worled');
+	//anser.classList.toggle('cosshow1');
+	//anser1.classList.toggle('cosshow2');
+	//anser2.classList.toggle('cosshow3');
+	//anser3.classList.toggle('cosshow4');
+	anser4.classList.toggle('cosshow5');
+});
